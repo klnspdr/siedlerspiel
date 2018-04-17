7315f5ea00dba478637d082dce012eab19df7495
nietsmmar <teabag.pof@gmail.com>
2018-04-17 10:15:42 +0200

    using jquery for building the matchfield to adjust it dynamically to the screen size. Also added sample space for a menu.

---
+++ js/main.js
@@ -0,0 +1,18 @@
+$(document).ready( function() {
+
+	buildMatchfield();
+	
+});
+
+function buildMatchfield() {
+	$( ".circle-container" ).height( $( "#matchfield" ).height() * 0.75);
+	$( ".circle-container" ).width( $( "#matchfield" ).height() * 0.75 );
+
+	var radius = $( "#matchfield" ).height() * 0.75/2;
+	$( ".deg0" ).css({'transform' : 'translate(' + radius + 'px'});
+	$( ".deg60" ).css({'transform' : 'rotate(60deg) translate(' + radius + 'px) rotate(-60deg) '});
+	$( ".deg120" ).css({'transform' : 'rotate(120deg) translate(' + radius + 'px) rotate(-120deg) '});
+	$( ".deg180" ).css({'transform' : 'translate(-' + radius + 'px'});
+	$( ".deg240" ).css({'transform' : 'rotate(240deg) translate(' + radius + 'px) rotate(-240deg) '});
+	$( ".deg300" ).css({'transform' : 'rotate(300deg) translate(' + radius + 'px) rotate(-300deg) '});
+}
