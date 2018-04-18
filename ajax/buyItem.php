a4762504570850604e7b8a84c2262a8e5ce94bed
nietsmmar <teabag.pof@gmail.com>
2018-04-18 03:13:27 +0200

    added useItem-method

--- ajax/buyItem.php
+++ ajax/buyItem.php
@@ -1,7 +1,6 @@
 <?php
 $shipId = $_POST["shipId"]+1;
 $item = $_POST["item"];
-$count = $_POST["count"] + 1;
 include("../connect.php"); //establish database connection   
 
 if ($item == "schiffswandverstaerkung") {
@@ -22,7 +21,7 @@ if ($item == "schiffswandverstaerkung") {
 	}
 }
 else {
-	$sql = "UPDATE inventory SET $item='$count' WHERE shipId=$shipId";
+	$sql = "UPDATE inventory SET $item=$item+1 WHERE shipId=$shipId";
 
 	if ($conn->query($sql) === TRUE) {
 	    echo true;
