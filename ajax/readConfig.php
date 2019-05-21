af4189454ecbc9cee623f44b15c849f63c58357a
kilianspeder <kilianspeder@icloud.com>
2019-05-21 20:13:46 +0200

    change numberItems to number_items and add $number_actions

--- ajax/readConfig.php
+++ ajax/readConfig.php
@@ -3,7 +3,8 @@
 $json_string = file_get_contents("../config/config.json");
 $config = json_decode($json_string, true);
 $number_groups=$config['number_groups'];
-$number_items=$config['numberItems'];
+$number_items=$config['number_items'];
+$number_actions=$config['number_actions'];
 if($config == null)
 	die("Error: invalid config file");
 ?>
