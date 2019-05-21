9f7e26367b2c2affbd4612ed6393fd00d43e7eb5
kilianspeder <kilianspeder@icloud.com>
2019-05-21 20:15:40 +0200

    add isset() at _GET condition

--- ajax/setRole.php
+++ ajax/setRole.php
@@ -1,7 +1,7 @@
 <?php
     if(isset($_POST['setRole'])) {
         $_SESSION['role'] = $_POST['setRole'];
-    } else if($_GET['setRole']){
+    } else if(isset($_GET['setRole'])){
         $_SESSION['role'] = $_GET['setRole'];
     }
 ?>
