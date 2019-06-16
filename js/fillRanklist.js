b1f2dcd81313c84b79aeaaa68f48b3808f4717f1
kilianspeder <kilianspeder@icloud.com>
2019-06-16 13:51:49 +0200

    use global variable wholeGroupData for ranklist now (no separate HTTP-request)

--- js/fillRanklist.js
+++ js/fillRanklist.js
@@ -1,25 +1,18 @@
 window.setInterval(function () {
     if (groupId == 100) {
 
-        $.getJSON("ajax/getGroupData.php")
-            .done(function(data){
-				data.sort((a,b) => (a['final_score'] > b['final_score']) ? 1 : ((b['final_score'] > a['final_score']) ? -1 : 0));
-                let sortedGroupData = data;
-                let tableContent = "<tbody id='rankListTableBody'>";
-                let rank = 1;
-                for(let i = sortedGroupData.length -1; i >= 0; i--){
-                    tableContent += "<tr><td>"+rank+"</td><td>" + sortedGroupData[i]['name'] + "</td><td>Punkte: "+ (sortedGroupData[i]['displayScore'] == true ? sortedGroupData[i]['final_score'] : "???") +"</td></tr>";
-                    rank++;
-                }
-                tableContent += "</tbody>";
-                $('#rankListTableBody').replaceWith(tableContent);
+        let sortedGroupData = wholeGroupData;
+        sortedGroupData.sort((a, b) => (a['final_score'] > b['final_score']) ? 1 : ((b['final_score'] > a['final_score']) ? -1 : 0));
 
-            })
-            .fail(function () {
-            });
+        let tableContent = "<tbody id='rankListTableBody'>";
+        let rank = 1;
+        for (let i = sortedGroupData.length - 1; i >= 0; i--) {
+            tableContent += "<tr><td>" + rank + "</td><td>" + sortedGroupData[i]['name'] + "</td><td>Punkte: " + (sortedGroupData[i]['displayScore'] == true ? sortedGroupData[i]['final_score'] : "???") + "</td></tr>";
+            rank++;
+        }
+        tableContent += "</tbody>";
+        $('#rankListTableBody').replaceWith(tableContent);
     }
-
-
 },1000);
 
 
