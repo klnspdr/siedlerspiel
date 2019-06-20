b2ae33fdd0b001d1fecf52ebd7d0ca7e478ef7ac
kilianspeder <kilianspeder@icloud.com>
2019-06-20 11:38:48 +0200

    fix wrong sorting of Groups

--- js/fillRanklist.js
+++ js/fillRanklist.js
@@ -2,12 +2,12 @@ window.setInterval(function () {
     if (groupId == 100) {
         if(wholeGroupData != null) {
 			let sortedGroupData = clone(wholeGroupData);
-            sortedGroupData.sort((a, b) => (a['final_score'] > b['final_score']) ? 1 : ((b['final_score'] > a['final_score']) ? -1 : 0));
+            sortedGroupData.sort((a, b) => (Number(a['final_score']) > Number(b['final_score'])) ? 1 : (Number((b['final_score']) > Number(a['final_score'])) ? -1 : 0));
 
             let tableContent = "<tbody id='rankListTableBody'>";
             let rank = 1;
             for (let i = sortedGroupData.length - 1; i >= 0; i--) {
-                tableContent += "<tr><td>" + rank + "</td><td>" + sortedGroupData[i]['name'] + "</td><td>Punkte: " + (sortedGroupData[i]['displayScore'] == true ? sortedGroupData[i]['final_score'] : "???") + "</td></tr>";
+                tableContent += "<tr><td id='rank'>" + rank + "</td><td id='groupName'>" + sortedGroupData[i]['name'] + "</td><td>Punkte: " + (sortedGroupData[i]['displayScore'] == true ? sortedGroupData[i]['final_score'] : "???") + "</td></tr>";
                 rank++;
             }
             tableContent += "</tbody>";
